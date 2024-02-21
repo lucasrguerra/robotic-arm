3487ecedcd4d7e8cf1021845f0cb250f532c4257
lucasrguerra <l.rayanguerra@gmail.com>
2024-02-21 19:55:49 -0300

    Update the code

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,24 @@
 
 
 
-#define CONTROL_BASE_PIN GPIO_NUM_32
-#define CONTROL_SHOULDER_PIN GPIO_NUM_35
-#define CONTROL_ELEVATION_PIN GPIO_NUM_33
-#define CONTROL_ELBOW_PIN GPIO_NUM_39
-#define CONTROL_WRIST_PIN GPIO_NUM_34
-#define CONTROL_HAND_PIN GPIO_NUM_36
+#define CONTROL_Y_1 GPIO_NUM_33
+#define CONTROL_X_1 GPIO_NUM_32
+#define CONTROL_Y_2 GPIO_NUM_35
+#define CONTROL_X_2 GPIO_NUM_34
+#define CONTROL_Y_3 GPIO_NUM_39
+#define CONTROL_X_3 GPIO_NUM_36
 
 #define SERVO_BASE_PIN GPIO_NUM_13
-#define SERVO_ELEVATION_PIN GPIO_NUM_12
-#define SERVO_SHOULDER_PIN GPIO_NUM_14
-#define SERVO_ELBOW_PIN GPIO_NUM_27
-#define SERVO_WRIST_PIN GPIO_NUM_26
-#define SERVO_HAND_PIN GPIO_NUM_25
+#define SERVO_SHOULDER_PIN GPIO_NUM_12
+#define SERVO_ELBOW_PIN GPIO_NUM_14
+#define SERVO_WRIST_PIN GPIO_NUM_27
+#define SERVO_HAND_PIN GPIO_NUM_26
+#define SERVO_CLAW_PIN GPIO_NUM_25
 
 #define SERVO_MAX_SIGNAL 2500
 #define SERVO_MIN_SIGNAL 500
 #define CONTROL_READ_DELAY 70
 #define SERVO_SET_DELAY 5
-#define SERVO_VELOCITY 1
 
 #define RESET_STATE_PIN GPIO_NUM_5
 #define TFT_SCL GPIO_NUM_18
@@ -47,6 +46,7 @@ typedef struct {
   uint8_t max_angle;
   uint8_t min_angle;
   bool inverted;
+  uint8_t velocity;
 
 
   void setup() {
@@ -62,12 +62,12 @@ typedef struct {
   }
 } ServoParameters;
 
-ServoParameters base = {Servo(), 90, CONTROL_BASE_PIN, SERVO_BASE_PIN, 180, 0, true};
-ServoParameters elevation = {Servo(), 60, CONTROL_ELEVATION_PIN, SERVO_ELEVATION_PIN, 180, 0, false};
-ServoParameters shoulder = {Servo(), 90, CONTROL_SHOULDER_PIN, SERVO_SHOULDER_PIN, 180, 0, true};
-ServoParameters elbow = {Servo(), 90, CONTROL_ELBOW_PIN, SERVO_ELBOW_PIN, 180, 0, true};
-ServoParameters wrist = {Servo(), 90, CONTROL_WRIST_PIN, SERVO_WRIST_PIN, 180, 0, false};
-ServoParameters hand = {Servo(), 90, CONTROL_HAND_PIN, SERVO_HAND_PIN, 150, 70, true};
+ServoParameters base = {Servo(), 90, CONTROL_X_1, SERVO_BASE_PIN, 180, 0, true, 1};
+ServoParameters shoulder = {Servo(), 40, CONTROL_Y_1, SERVO_SHOULDER_PIN, 180, 0, false, 1};
+ServoParameters elbow = {Servo(), 110, CONTROL_Y_3, SERVO_ELBOW_PIN, 180, 0, false, 1};
+ServoParameters wrist = {Servo(), 135, CONTROL_Y_2, SERVO_WRIST_PIN, 180, 0, false, 1};
+ServoParameters hand = {Servo(), 90, CONTROL_X_2, SERVO_HAND_PIN, 180, 0, false, 1};
+ServoParameters claw = {Servo(), 90, CONTROL_X_3, SERVO_CLAW_PIN, 160, 70, false, 5};
 
 Adafruit_ST7789 display = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
@@ -96,11 +96,11 @@ void setup() {
 
 
   base.setup();
-  elevation.setup();
   shoulder.setup();
   elbow.setup();
   wrist.setup();
   hand.setup();
+  claw.setup();
 
 
   BaseType_t task_read_control_base = xTaskCreate(read_control, "read_control_base", (configMINIMAL_STACK_SIZE + 256), &base, 2, NULL);
@@ -108,12 +108,6 @@ void setup() {
 
   BaseType_t task_set_servo_base = xTaskCreate(set_servo, "set_servo_base", (configMINIMAL_STACK_SIZE), &base, 1, NULL);
   if (task_set_servo_base == pdFAIL) { Serial.println("Task set_servo_base failed to create"); }
-
-  BaseType_t task_read_control_elevation = xTaskCreate(read_control, "read_control_elevation", (configMINIMAL_STACK_SIZE + 256), &elevation, 2, NULL);
-  if (task_read_control_elevation == pdFAIL) { Serial.println("Task read_control_elevation failed to create"); }
-
-  BaseType_t task_set_servo_elevation = xTaskCreate(set_servo, "set_servo_elevation", (configMINIMAL_STACK_SIZE), &elevation, 1, NULL);
-  if (task_set_servo_elevation == pdFAIL) { Serial.println("Task set_servo_elevation failed to create"); }
 
   BaseType_t task_read_control_shoulder = xTaskCreate(read_control, "read_control_shoulder", (configMINIMAL_STACK_SIZE + 256), &shoulder, 2, NULL);
   if (task_read_control_shoulder == pdFAIL) { Serial.println("Task read_control_shoulder failed to create"); }
@@ -139,6 +133,12 @@ void setup() {
   BaseType_t task_set_servo_hand = xTaskCreate(set_servo, "set_servo_hand", (configMINIMAL_STACK_SIZE), &hand, 1, NULL);
   if (task_set_servo_hand == pdFAIL) { Serial.println("Task set_servo_hand failed to create"); }
 
+  BaseType_t task_read_control_claw = xTaskCreate(read_control, "read_control_claw", (configMINIMAL_STACK_SIZE + 256), &claw, 2, NULL);
+  if (task_read_control_claw == pdFAIL) { Serial.println("Task read_control_hand failed to create"); }
+
+  BaseType_t task_set_servo_claw = xTaskCreate(set_servo, "set_servo_claw", (configMINIMAL_STACK_SIZE), &claw, 1, NULL);
+  if (task_set_servo_claw == pdFAIL) { Serial.println("Task set_servo_claw failed to create"); }
+
   BaseType_t task_check_reset_state = xTaskCreate(check_reset_state, "check_reset_state", (configMINIMAL_STACK_SIZE + 1024), NULL, 3, NULL);
   if (task_check_reset_state == pdFAIL) { Serial.println("Task check_reset_state failed to create"); }
 
@@ -162,11 +162,11 @@ void read_control(void *pvParameters) {
     int16_t new_angle = parameters->angle;
 
     if (control_value <= 1000) {
-      new_angle -= parameters->inverted ? SERVO_VELOCITY : -SERVO_VELOCITY;
+      new_angle -= parameters->inverted ? parameters->velocity : -parameters->velocity;
     }
 
     if (control_value >= 3000) {
-      new_angle += parameters->inverted ? SERVO_VELOCITY : -SERVO_VELOCITY;
+      new_angle += parameters->inverted ? parameters->velocity : -parameters->velocity;
     }
   
     if (new_angle < parameters->min_angle) { new_angle = parameters->min_angle; }
@@ -196,11 +196,11 @@ void check_reset_state(void *pvParameters) {
     bool reset_state = digitalRead(RESET_STATE_PIN);
     if (reset_state == LOW) {
       base.angle = 90;
-      elevation.angle = 60;
       shoulder.angle = 90;
       elbow.angle = 90;
       wrist.angle = 90;
       hand.angle = 90;
+      claw.angle = 90;
     }
     vTaskDelay(20);
   }
@@ -221,18 +221,13 @@ void update_display(void *pvParameters) {
     display.println(base.angle);
 
     display.setTextColor(ST77XX_WHITE);
-    display.print("Elevation:      ");
-    display.setTextColor(ST77XX_GREEN);
-    display.println(elevation.angle);
-
-    display.setTextColor(ST77XX_WHITE);
     display.print("Shoulder:       ");
-    display.setTextColor(ST77XX_BLUE);
+    display.setTextColor(ST77XX_GREEN);
     display.println(shoulder.angle);
 
     display.setTextColor(ST77XX_WHITE);
     display.print("Elbow:          ");
-    display.setTextColor(ST77XX_YELLOW);
+    display.setTextColor(ST77XX_BLUE);
     display.println(elbow.angle);
 
     display.setTextColor(ST77XX_WHITE);
@@ -242,8 +237,13 @@ void update_display(void *pvParameters) {
 
     display.setTextColor(ST77XX_WHITE);
     display.print("Hand:           ");
-    display.setTextColor(ST77XX_MAGENTA);
+    display.setTextColor(ST77XX_YELLOW);
     display.println(hand.angle);
+
+    display.setTextColor(ST77XX_WHITE);
+    display.print("Claw:           ");
+    display.setTextColor(ST77XX_MAGENTA);
+    display.println(claw.angle);
 
     vTaskDelay(50);
   }
